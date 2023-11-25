6af79c4f7ba4c0bc5349ef32c491aad70bd725ca
AdityaSharmajr <149559271+AdityaSharmajr@users.noreply.github.com>
2023-11-25 13:18:56 +0530

    Add files via upload

---
+++ 03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score1 = 45
+let score2 = "45"
+let score3 = "45abc"
+console.log(typeof score1);
+console.log(typeof(score2));
+
+// let valueInNumber = Number(score3)
+// console.log(valueInNumber);
+
+
+// "45" = 45
+// "45bc" = => NaN
+// true =>;  false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Aditya" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ 05_String.js
@@ -0,0 +1,175 @@
+const name = "aditya"
+const repocount = 2
+
+// console.log(name + repocount + " Value");
+
+// console.log(`Hello my name is ${name} and my 
+// repo count is ${repocount}`);
+
+const gameName = new String('Aditya-ttt-tt');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// // 
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-4, 4)
+// console.log(anotherString);
+
+const newStringOne = "    Aditya    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://Aditya.com/aditya%21Sharma"
+
+// console.log(url.replace('%21','_'));
+
+// console.log(url.includes('Kohinoor'));
+
+// console.log(gameName.split('-'));
+
+
+//*******************String Methods****************************
+
+let StrMethodPra = "Aditya Sharma"
+let Str2Pra = "Hero"
+console.log(typeof StrMethodPra);
+
+// anchor()
+console.log(StrMethodPra.anchor());
+// at()
+console.log(StrMethodPra.at());
+// big
+console.log(StrMethodPra.big());
+// blink
+console.log(StrMethodPra.blink());
+// bold
+console.log(StrMethodPra.bold());
+// charAt
+console.log(StrMethodPra.charAt(2));
+// charCodeAt
+console.log(StrMethodPra.charCodeAt(2));
+// codePointAt
+console.log(StrMethodPra.codePointAt(0));
+// concat
+console.log(StrMethodPra.concat(Str2Pra));
+// constructor
+console.log(StrMethodPra.constructor);
+// endsWith
+console.log(StrMethodPra.endsWith("Sharma"));
+// fixed
+console.log(StrMethodPra.fixed());
+// fontcolor
+console.log(StrMethodPra.fontcolor());
+// fontsize
+console.log(StrMethodPra.fontsize());
+// includes
+console.log(StrMethodPra.includes("Sharma"));
+// indexOf
+console.log(StrMethodPra.indexOf("A"));
+// isWellFormed
+console.log(StrMethodPra.isWellFormed());
+// italics
+console.log(StrMethodPra.italics());
+// lastIndexOf
+console.log(StrMethodPra.lastIndexOf("Sharma"));
+// length
+console.log(StrMethodPra.length);
+// link
+console.log(StrMethodPra.link());
+// localeCompare
+console.log(StrMethodPra.localeCompare(Str2Pra));
+// match
+console.log(StrMethodPra.match());
+// matchAll
+console.log(StrMethodPra.matchAll());
+// normalize
+console.log(StrMethodPra.normalize());
+// padEnd
+console.log(StrMethodPra.padEnd());
+// padStart
+// : 
+// ƒ padStart()
+// repeat
+// : 
+// ƒ repeat()
+// replace
+// : 
+// ƒ replace()
+// replaceAll
+// : 
+// ƒ replaceAll()
+// search
+// : 
+// ƒ search()
+// slice
+// : 
+// ƒ slice()
+// small
+// : 
+// ƒ small()
+// split
+// : 
+// ƒ split()
+// startsWith
+// : 
+// ƒ startsWith()
+// strike
+// : 
+// ƒ strike()
+// sub
+// : 
+// ƒ sub()
+// substr
+// : 
+// ƒ substr()
+// substring
+// : 
+// ƒ substring()
+// sup
+// : 
+// ƒ sup()
+// toLocaleLowerCase
+// : 
+// ƒ toLocaleLowerCase()
+// toLocaleUpperCase
+// : 
+// ƒ toLocaleUpperCase()
+// toLowerCase
+// : 
+// ƒ toLowerCase()
+// toString
+// : 
+// ƒ toString()
+// toUpperCase
+// : 
+// ƒ toUpperCase()
+// toWellFormed
+// : 
+// ƒ toWellFormed()
+// trim
+// : 
+// ƒ trim()
+// trimEnd
+// : 
+// ƒ trimEnd()
+// trimLeft
+// : 
+// ƒ trimStart()
+// trimRight
+// : 
+// ƒ trimEnd()
+// trimStart
+// : 
+// ƒ trimStart()
+// valueOf
+// : 
+// ƒ valueOf()
+// Symbol(Symbol.iterator)
